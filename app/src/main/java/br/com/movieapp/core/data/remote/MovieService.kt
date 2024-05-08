2409e08a4ea86d66c874434786e3b177644c816d
Gabriel Benatti <gabriel@interno.infonacci.com.br>
2024-05-08 17:43:52 -0300

    adicionado movie service (incompleto)

---
+++ app/src/main/java/br/com/movieapp/core/data/remote/MovieService.kt
@@ -0,0 +1,25 @@
+package br.com.movieapp.core.data.remote
+
+import br.com.movieapp.core.data.remote.response.MovieResponse
+import br.com.movieapp.core.data.remote.response.SearchResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieService {
+    @GET("movie/popular")
+    suspend fun getPropularMovies(
+        @Query("page") page: Int
+    ): MovieResponse
+
+    @GET("movie/multi")
+    suspend fun searchMovie(
+        @Query("page") page: Int,
+        @Query("query") query: String
+    ): SearchResponse
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovie(
+        @Path("movie_id") movieId: Int
+    ): MovieResponse
+}
