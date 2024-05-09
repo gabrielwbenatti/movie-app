e40e6958c135b56cbab2e457a59aa008382e7c46
Gabriel Benatti <gabriel@interno.infonacci.com.br>
2024-05-09 16:32:22 -0300

    criado build config e params interceptor

---
+++ app/src/main/java/br/com/movieapp/core/util/Constants.kt
@@ -0,0 +1,8 @@
+package br.com.movieapp.core.util
+
+object Constants {
+    const val LANGUAGE_PARAM = "language"
+    const val LANGUAGE_VALUE = "pt-BR"
+
+    const val API_KEY_PARAM = "api_key"
+}
